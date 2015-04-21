1ea67f76fc3d7c90c649b082ea28d1371b210657
Enrique <enrique.arauz@gmail.com>
2015-04-21 11:24:23 +0200

    Refinada la simulación de autobuses mediante Threads

--- ISI Práctica 1 - Simulador/src/simulador/Autobus.java
+++ ISI Práctica 1 - Simulador/src/simulador/Autobus.java
@@ -2,15 +2,15 @@ package simulador;
 
 import java.util.Random;
 
-public class Autobus {
-	private int id;
+public class Autobus extends Thread{
+	private String busId;
 	private double latitud;
 	private double longitud;
-	private static final double bordeInferior = 33.3333333;
+	private static final double bordeInferior = 3.3333333;
 	private static final double bordeSuperior = 35.3333333;
 
-	public Autobus(int id) {
-		this.id = id;
+	public Autobus(String id) {
+		this.busId = id;
 		latitud = longitud = (bordeInferior + bordeSuperior) / 2;
 	}
 
@@ -22,8 +22,8 @@ public class Autobus {
 		return longitud;
 	}
 
-	public int getId() {
-		return id;
+	public String getBusId() {
+		return busId;
 	}
 
 	public static double getBordeinferior() {
@@ -33,28 +33,47 @@ public class Autobus {
 	public static double getBordesuperior() {
 		return bordeSuperior;
 	}
+	
+	public void atasco() throws InterruptedException{
+		System.out.println("El bus " + busId + " está en un atasco");
+		this.sleep(60000);
+	}
 
-	public void simularLocalizacion() {
-		int desplazamiento = new Random().nextInt(4);
-		switch (desplazamiento) {
-		case 0:
-			latitud += 0.002;
-			break;
-		case 1:
-			latitud -= 0.002;
-			break;
-		case 2:
-			longitud += 0.002;
-			break;
-		case 3:
-			longitud += 0.002;
-			break;
+	public void run() {
+		while(true){
+			int desplazamiento = new Random().nextInt(4);
+			int atascoRandom = new Random().nextInt(3);
+			if(atascoRandom%3 == 1){
+				try {
+					atasco();
+				} catch (InterruptedException e) {}
+			} else {
+				switch (desplazamiento) {
+					case 0:
+						longitud += 0.01;
+						latitud += 0.02;
+						break;
+					case 1:
+						longitud += 0.02;
+						latitud -= 0.01;
+						break;
+					case 2:
+						longitud += 0.03;
+						latitud += 0.02;
+						break;
+					case 3:
+						longitud -= 0.02;
+						latitud += 0.04;
+						break;
+				}
+			}
 		}
 	}
 
 	public String toString() {
-		return "Bus #" + id + ". Latitud: " + latitud + ". Longitud: "
+		return "Bus " + busId + "\nLatitud: " + latitud + "\nLongitud: "
 				+ longitud + ".";
 
 	}
 }
+
---
+++ ISI Práctica 1 - Simulador/src/simulador/Linea.java
@@ -0,0 +1,58 @@
+package simulador;
+
+import com.angryelectron.thingspeak.Channel;
+import com.angryelectron.thingspeak.Entry;
+import com.angryelectron.thingspeak.ThingSpeakException;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+public class Linea extends Thread{
+	private Autobus[] flota;
+	private int numero;
+	private String apiKey;
+	private Channel canal;
+	private Entry entrada;
+	
+	public Linea(int n, int buses, int canalNumero, String cadena){
+		numero = n;
+		flota = new Autobus[buses];
+		apiKey = cadena;
+		canal = new Channel(canalNumero,apiKey);
+		entrada = new Entry();
+	}
+	
+	public void inicializarFlota(){
+		for(int i = 0; i < flota.length; i++){
+			flota[i] = new Autobus("L" + numero + "B0" + (i+1));
+		}
+		for(Autobus bus : flota){
+			bus.start();
+		}
+	}
+	
+	public void run(){
+		while(true){
+			for(Autobus bus: flota){
+				entrada.setField(1, bus.getBusId());
+				entrada.setField(2, Double.toString(bus.getLatitud()));
+				entrada.setField(3, Double.toString(bus.getLongitud()));
+				try {
+					canal.update(entrada);
+				} catch (UnirestException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (ThingSpeakException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				System.out.println(bus.toString());
+				try {
+					this.sleep(16000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
