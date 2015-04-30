3e9c8b05f53ec2f1d853bda40ac20e33b4365b1d
Ramón Ramírez González <rrgonzalez1992@gmail.com>
2015-04-30 12:21:44 +0200

    Eliminados Warnings

--- ISI_REST/src/serviciosBusNow/Prueba.java
+++ ISI_REST/src/serviciosBusNow/Prueba.java
@@ -5,10 +5,11 @@ import java.util.ArrayList;
 
 public class Prueba {
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) throws UnknownHostException {
 		ConfigBD configBD = new ConfigBD();
 		Operaciones operaciones = new Operaciones();
-		ArrayList<Autobus> buses = operaciones.obtenerBusesDisponibles("L1B05");
+		ArrayList<Autobus> buses = Operaciones.obtenerBusesDisponibles("L1B05");
 		for (Autobus bus: buses){
 			System.out.println(bus.getBusID());
 		}
